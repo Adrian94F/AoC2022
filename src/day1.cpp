b704af09b7901bb5b82504bc1f630651e260b766
Adrian94F <adrian.frydmanski@hotmail.com>
2022-12-01 12:30:35 +0100

    Day 1 optimization

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,37 +1,45 @@
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
-#include <bits/stdc++.h>
 
 using namespace std;
 
+vector<int> top_3_elves = {0, 0, 0};
+auto calories = 0;
+
+void check_calories() {
+    auto min_value = min_element(top_3_elves.begin(), top_3_elves.end());
+    if (calories > *min_value) {
+        top_3_elves.erase(min_value);
+        top_3_elves.push_back(calories);
+    }
+}
 
 int main() {
     ifstream input("./day1.txt");
-
-    if (input.is_open()) {
-        string line;
-        vector<int> elves;
-        auto calories = 0;
-
-        while ( getline (input,line) ) {
-            if (line.empty()) {
-                elves.push_back(calories);
-                calories = 0;
-            }
-            else {
-                calories += stoi(line);
-            }
-        }
-        elves.push_back(calories);
-        input.close();
-
-        sort(elves.begin(), elves.end(), greater<>());
-
-        cout << "Max calories sum: " << elves[0] << endl;
-        cout << "Top 3 elves calories sum: " << accumulate(elves.begin(), elves.begin()+3, 0) << endl;
-    }
-    else {
+    if (!input.is_open()) {
         cout << "Unable to open file";
+        return -1;
     }
+
+    string line;
+    while (getline(input,line)) {
+        if (line.empty()) {
+            check_calories();
+            calories = 0;
+        }
+        else {
+            calories += stoi(line);
+        }
+    }
+    check_calories();
+    input.close();
+
+    auto max = *max_element(top_3_elves.begin(), top_3_elves.end());
+    auto sum = accumulate(top_3_elves.begin(), top_3_elves.end(), 0);
+
+    cout << "Max calories sum: " << max << endl;
+    cout << "Top " << top_3_elves.size() << " elves calories sum: " << sum << endl;
+
     return 0;
 }
