793475c6eec2b66b5d5cbdfbba7bad0293bab683
Adrian94F <adrian.frydmanski@hotmail.com>
2022-12-06 16:13:15 +0100

    Day4

---
+++ src/day4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int day4() {
+    ifstream input("./day4.txt");
+    if (!input.is_open()) {
+        cout << "Unable to open file";
+        return -1;
+    }
+
+    auto pairs_containing_other = 0;
+    auto overlapping_pairs = 0;
+    string line;
+    while (getline(input, line)) {
+        string pair_delimiter = ",";
+        string range_delimiter = "-";
+        auto range1 = line.substr(0, line.find(pair_delimiter));
+        auto range2 = line.substr(line.find(pair_delimiter) + pair_delimiter.length(), line.length());
+        auto r1_delimiter = range1.find(range_delimiter);
+        auto r2_delimiter = range2.find(range_delimiter);
+        auto r1_0 = stoi(range1.substr(0, r1_delimiter));
+        auto r1_1 = stoi(range1.substr(r1_delimiter + range_delimiter.size(), range1.length()));
+        auto r2_0 = stoi(range2.substr(0, r2_delimiter));
+        auto r2_1 = stoi(range2.substr(r2_delimiter + range_delimiter.size(), range2.length()));
+
+        if ((r1_0 <= r2_0 and r1_1 >= r2_1) or (r1_0 >= r2_0 and r1_1 <= r2_1)) {
+            pairs_containing_other++;
+        }
+
+        if (not(r1_0 > r2_1 or r1_1 < r2_0)) {
+            overlapping_pairs++;
+        }
+    }
+    input.close();
+
+    cout << "Pairs containing other pair: " << pairs_containing_other << endl;
+    cout << "Overlapping pairs: " << overlapping_pairs << endl;
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include "day1.cpp"
 #include "day2.cpp"
 #include "day3.cpp"
+#include "day4.cpp"
 
 int main() {
     auto functions = {
             day1,
             Day2::run,
-            day3
+            day3,
+            day4
     };
     /*for (auto f : functions) {
         f();
