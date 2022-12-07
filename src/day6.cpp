43a7ae8120ee4d4a478f71e2d6fbc4bd757d41b8
Adrian94F <adrian.frydmanski@hotmail.com>
2022-12-07 23:06:03 +0100

    Day 6

--- src/day6.cpp
+++ src/day6.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 int day6() {
     ifstream input("./day6.txt");
     if (!input.is_open()) {
@@ -13,7 +12,23 @@ int day6() {
 
     string line;
     while (getline(input,line)) {
-
+        const map<int, string> marker_types = {
+                {4, "packet"},
+                {14, "message"}
+        };
+        for (const auto& m : marker_types) {
+            const auto marker_characters = m.first;
+            for (auto i = 0; i < line.size() - marker_characters; i++) {
+                set<char> marker;
+                for (auto j = 0; j < marker_characters; j++) {
+                    marker.insert(line[i + j]);
+                }
+                if (marker.size() == marker_characters) {
+                    cout << "start of " << m.second << " marker: " << i + marker_characters << endl;
+                    break;
+                }
+            }
+        }
     }
     input.close();
 
