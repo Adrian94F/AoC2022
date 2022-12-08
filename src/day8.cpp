5bde9feaf14896485abde7eb463ffe4bef6d087e
Adrian94F <adrian.frydmanski@hotmail.com>
2022-12-08 10:37:44 +0100

    Day 8 init

---
+++ src/day8.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int day8() {
+    ifstream input("./day8.txt");
+    if (!input.is_open()) {
+        cout << "Unable to open file";
+        return -1;
+    }
+
+    string line;
+    while (getline(input,line)) {
+
+    }
+    input.close();
+
+    return 0;
+}
